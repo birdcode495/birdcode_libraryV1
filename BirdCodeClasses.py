b733521820bb43a5d1f1b002b0b03f20af39f9e0
birdcode495 <birdcode.py@gmail.com>
2024-04-27 11:03:46 -0500

    Creation of the subclass Apodiformes

--- BirdCodeClasses.py
+++ BirdCodeClasses.py
@@ -18,12 +18,13 @@ class Aves():
 
 	def __init__(self):
 
-		self.__scientificName = 'Aves'
+		self.__class_scientificName = 'Aves'
 		self.__class_englishName = 'Birds'
 		self.__class_spanishName = 'Aves'
 		self.__class_frenchname = ''
 		self.__class_germanName = ''
 		self.__class_chineseName = '鸟类'
+		self.__class_speciesNumber_gbif = 14825
 		self.__vertebralColumn = True
 		self.__homeothermy = True
 		self.__forelimbs = 'wings'
@@ -32,7 +33,7 @@ class Aves():
 		self.__teeth = False
 		self.__animalReproduction = 'oviparous'
 		self.__temporalRange_ma = 72
-		self.__specialty = 'Ornithology'
+		self.__speciality = 'Ornithology'
 		self.__digestiveSystem = 'crop and gizzard'
 		self.__sexChromosome = 'Z, W'
 		self.__femaleChromosome = '(ZW)'
@@ -40,6 +41,8 @@ class Aves():
 		self.__sweatGland = False
 		self.__flight = True
 		self.__flying = False
+
+	
 
 	def takeOff(self, letsFly):
 		
@@ -57,6 +60,36 @@ class Aves():
 		print("The integumentary system of Mulsant 1 is: ", self.__integumentarySystem)
 		print("The forelimbs of Mulsant 1 are: " , self.__forelimbs)
 		print("The mouth type of Mulsant 1 is: ", self.__mouthType)
+
+
+
+
+# --------------------------------------- Creation of the class Apodiformes ----------------------------------
+
+
+class Apodiformes(Aves):
+
+	self.__order_scientificName = 'Apodiformes'
+	self.__englishName = ''
+	self.__spanishName = ''
+	self.__frenchName = ''
+	self.__germanName = ''
+	self.__chineseName = ''
+	self.__order_speciesNumber_gbif = 541
+
+
+
+
+
+
+
+# ------------------------------------- Creation of the class Trochilidae ------------------------------------
+
+
+
+
+
+
 
 
 # ------------------------------------------- Object creation -----------------------------------------------
@@ -87,4 +120,4 @@ print()
 
 
 
-
+# ----------------------------------------- Creation of the class T
