203b2e5d93777cfd937ac6d21b111a20a62efd16
birdcode495 <birdcode.py@gmail.com>
2024-04-27 07:45:06 -0500

    Definition of the class Birds

---
+++ BirdCodeClasses.py
@@ -0,0 +1,19 @@
+class Aves():
+
+	vertebralColumn = True
+	homeothermy = True
+	forelimbs = 'wings'
+	integumentarySystem = 'feathers'
+	mouthType = 'beak'
+	teeth = False
+	animalReproduction = 'oviparous'
+	temporalRange_ma = 72
+	specialty = 'Ornithology'
+	digestiveSystem = 'crop and gizzard'
+	sexChromosome = 'Z, W'
+	femalesChromosome = '(ZW)'
+	maleChromosome = '(ZZ)'
+	sweatGland = False
+	flight = True
+	flying= False
+
